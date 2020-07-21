dd3022bd010f968d53dc8152284a4a5f51efc116
DimaLabsdeveloper <66136912+DimaLabsdeveloper@users.noreply.github.com>
2020-07-21 16:16:26 +0300

    Add files via upload

---
+++ Addword.xaml.cs
@@ -0,0 +1,107 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace applernnewwords
+{
+    /// <summary>
+    /// Логика взаимодействия для Addword.xaml
+    /// </summary>
+    public partial class Addword : Window
+    {
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+        public Addword()
+        {
+            InitializeComponent();
+        }
+
+        private void myword_Click(object sender, RoutedEventArgs e)
+        {
+            Myword k = new Myword();
+            this.Close();
+            k.Show();
+        }
+
+        private void newword_Click(object sender, RoutedEventArgs e)
+        {
+            Enwords y = new Enwords();
+            this.Close();
+            y.Show();
+        }
+
+        private void proprog_Click(object sender, RoutedEventArgs e)
+        {
+            Proprog j = new Proprog();
+            this.Close();
+            j.Show();
+        }
+
+        private void sett_Click(object sender, RoutedEventArgs e)
+        {
+            Settp k = new Settp();
+            this.Close();
+            k.Show();
+        }
+
+        private void ireg_Click(object sender, RoutedEventArgs e)
+        {
+            Iragwen h = new Iragwen();
+            this.Close();
+            h.Show();
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            if (ukrword.Text == "" && englword.Text == "") { erreor.Content = "                 введіть слова"; }
+            else if (ukrword.Text == "") { erreor.Content = "            введіть українське слово"; }
+            else if (englword.Text == "") { erreor.Content = "            введіть англійське слово"; }
+            else
+            {
+                MessageBox.Show("слово додано!");
+                erreor.Content = "    ";
+                Myword g = new Myword();
+                this.Close();
+                g.Show();
+
+
+
+                string cs = @"server=localhost;userid=admin;password=admin;database=word";
+
+              
+                string CommandText = "INSERT INTO words (ukr,en) VALUES ('" + ukrword.Text + "','" + englword.Text + "');";
+                using (MySqlConnection myConnection = new MySqlConnection(cs))
+                {
+
+                    using (MySqlCommand myCommand = new MySqlCommand(CommandText, myConnection))
+                    {
+
+                        myConnection.Open();
+
+                        myCommand.ExecuteNonQuery();
+
+                    }
+                }
+            }
+        }
+      
+    }
+}
---
+++ Lernuk.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace applernnewwords
+{
+    /// <summary>
+    /// Логика взаимодействия для Lernuk.xaml
+    /// </summary>
+    public partial class Lernuk : Window
+    {
+        public Lernuk()
+        {
+            InitializeComponent();
+            englbutton.Visibility = Visibility.Hidden;
+        }
+
+        private void home_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow o = new MainWindow();
+            o.Show();
+            this.Close();
+        }
+
+        private void ireg_Click(object sender, RoutedEventArgs e)
+        {
+
+            Iragwen q = new Iragwen();
+            this.Close();
+            q.Show();
+        }
+
+        private void myword_Click(object sender, RoutedEventArgs e)
+        {
+            Myword k = new Myword();
+            k.Show();
+            this.Close();
+        }
+
+        
+
+       
+
+        private void R1_Click(object sender, RoutedEventArgs e)
+        {
+            englbutton.Visibility = Visibility.Visible;
+            text.Content = "натисніть на слово щоб побатити переклад";
+
+            string cs = @"server=localhost;userid=admin;password=admin;database=word";
+
+            
+            using (MySqlConnection cn = new MySqlConnection(cs))
+            {
+                
+
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM word"))
+                {
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    using (SqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+
+                        if (dataReader.HasRows)
+                        {
+                          
+                            while (dataReader.Read())
+                            {
+                                englbutton.Content =(dataReader.GetString(0));
+                            }
+                            
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+
+        private void newword_Click(object sender, RoutedEventArgs e)
+        {
+            Enwords h = new Enwords();
+            h.Show();
+            this.Close();
+        }
+
+        private void proprog_Click(object sender, RoutedEventArgs e)
+        {
+            Proprog j = new Proprog();
+            this.Close();
+            j.Show();
+        }
+
+        private void sett_Click(object sender, RoutedEventArgs e)
+        {
+            Settp p = new Settp();
+            this.Close();
+            p.Show();
+        }
+
+        private void englbutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
